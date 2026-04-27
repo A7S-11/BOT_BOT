ee23eced3746ea8cf39bb451f4aea84b715e98bf
A7S-11 <hdjehbl@gmail.com>
2026-04-27 04:17:13 +0300

    Delete project directory

--- project/config.py
+++
@@ -1,9 +0,0 @@
-import os
-API_ID = int(os.getenv("API_ID"))
-ABI_HASH = os.getenv("ABI_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENAI_API = os.getenv("OPENAI_API")
-
-DELAY_MIN = 60
-DELAY_MAX = 120
-REPLY_PROBABILITY = 0.6
